13c4c05e73d8fbb8dce0100c5b571e95c3207f7c
Nazmul Alom <nazmulshuvo03@gmail.com>
2025-05-23 18:14:05 +0600

    project init

---
+++ main.py
@@ -0,0 +1,68 @@
+import json
+
+from dotenv import load_dotenv
+from livekit import agents
+from livekit.agents import Agent, AgentSession, RoomInputOptions
+from livekit.plugins import (cartesia, deepgram, google, noise_cancellation,
+                             silero)
+# from livekit.plugins.turn_detector.multilingual import MultilingualModel
+from livekit.plugins.turn_detector.english import EnglishModel
+
+load_dotenv()
+
+
+class Assistant(Agent):
+    def __init__(self, instructions: str = "") -> None:
+        super().__init__(instructions=instructions)
+
+
+async def entrypoint(ctx: agents.JobContext):
+    await ctx.connect()
+
+    # Parse metadata instructions
+    metadata = json.loads(ctx.room.metadata or "{}")
+    instructions = metadata.get("instructions", "You are a helpful assistant.")
+
+    agent = Assistant(instructions=instructions)
+
+    # session = AgentSession(
+    #     llm=google.beta.realtime.RealtimeModel(
+    #         model="gemini-2.0-flash-exp",
+    #         voice="Puck",
+    #     ),
+    # )
+
+    session = AgentSession(
+        stt=deepgram.STT(language="en"),
+        # llm=google.LLM(
+        #     model="gemini-2.0-flash-exp",
+        #     temperature=0.8,
+        # ),
+        llm=google.beta.realtime.RealtimeModel(
+            model="gemini-2.0-flash-exp",
+            voice="Puck",
+            # temparature=0.8,
+        ),
+        tts=cartesia.TTS(),
+        vad=silero.VAD.load(),
+        # turn_detection=MultilingualModel(),
+        turn_detection=EnglishModel(),
+    )
+    await session.start(
+        room=ctx.room,
+        agent=agent,
+        room_input_options=RoomInputOptions(
+            # LiveKit Cloud enhanced noise cancellation
+            # - If self-hosting, omit this parameter
+            # - For telephony applications, use `BVCTelephony` for best results
+            noise_cancellation=noise_cancellation.BVC(),
+        ),
+    )
+
+    await session.generate_reply(
+        instructions="Greet the user as an interviewee and ask questions."
+    )
+
+
+if __name__ == "__main__":
+    agents.cli.run_app(agents.WorkerOptions(entrypoint_fnc=entrypoint))
